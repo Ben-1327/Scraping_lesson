6e7eb08863417aa0f00d999f4399ee649c3fc4f0
Ben1327 <ff.walker1327@gmail.com>
2020-11-11 07:56:23 +0900

    [Add] open google

---
+++ selenium_lesson.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome('/usr/local/Caskroom/chromedriver/86.0.4240.22/chromedriver')
+
+driver.get('https://www.google.co.jp')
