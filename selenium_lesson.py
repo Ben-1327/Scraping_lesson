be4a355276a059162d2e2b66590bee476c01f2ef
Ben1327 <ff.walker1327@gmail.com>
2020-11-11 07:57:32 +0900

    [Add] print result of search

--- selenium_lesson.py
+++ selenium_lesson.py
@@ -9,3 +9,8 @@ driver.get('https://www.google.co.jp')
 search_bar = driver.find_element_by_name("q")
 search_bar.send_keys("python")
 search_bar.submit()
+
+for elem_h3 in driver.find_elements_by_xpath('//a/h3'):
+    elem_a = elem_h3.find_element_by_xpath('..')
+    print(elem_h3.text)
+    print(elem_a.get_attribute('href'))
