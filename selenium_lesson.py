6c81494929d0fa97499c93a534463e499c264c47
Ben1327 <ff.walker1327@gmail.com>
2020-11-11 07:56:58 +0900

    [Add] search on google

--- selenium_lesson.py
+++ selenium_lesson.py
@@ -4,3 +4,8 @@ from time import sleep
 driver = webdriver.Chrome('/usr/local/Caskroom/chromedriver/86.0.4240.22/chromedriver')
 
 driver.get('https://www.google.co.jp')
+
+
+search_bar = driver.find_element_by_name("q")
+search_bar.send_keys("python")
+search_bar.submit()
